9dbf819f0e90786ce4b90e84ba57009a7c1928ac
King Leo <746654+lionheart06@users.noreply.github.com>
2026-02-23 14:19:59 -0800

    cleaning up

---
+++ kitkatTests/kitkatTests.swift
@@ -0,0 +1,16 @@
+//
+//  kitkatTests.swift
+//  kitkatTests
+//
+//  Created by Richard El-Kadi on 2/23/26.
+//
+
+import Testing
+
+struct kitkatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
